d5613aa6f99a0f792ab496add8c54f5af9df1282
Peter Maila <82019073+Cephas-Peter@users.noreply.github.com>
2026-09-09 16:24:12 +0200

    Create silver_script.py

---
+++ silver_script.py
@@ -0,0 +1,13 @@
+df_silver = (
+    df_bronze
+    .filter(col("event_type") == "purchase")
+    .dropna(subset=["customer_id", "amount"])
+    .withColumn("event_date", to_date(col("event_timestamp")))
+    .withColumn("payment_method", lower(col("payment_method")))
+    .withColumn("amount", col("amount").cast("float"))
+    .select(
+        "event_id", "customer_id", "event_date", "product_id",
+        "product_category", "payment_method", "amount", "location"
+    )
+)
+df_silver.write.mode("overwrite").parquet("abfss://retailcontainer@retaildatalake.dfs.core.windows.net/silver/")
